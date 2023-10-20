60c620a7165e4a4884b5c4728a56d301438c0497
Rohan Sharma <117426013+RS-labhub@users.noreply.github.com>
2023-10-20 16:42:51 +0530

    Create Upload Files.cjs

---
+++ Upload Files.cjs
@@ -0,0 +1,10 @@
+var http = require('http');
+
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.write('<form action="fileupload" method="post" enctype="multipart/form-data">');
+  res.write('<input type="file" name="filetoupload"><br>');
+  res.write('<input type="submit">');
+  res.write('</form>');
+  return res.end();
+}).listen(8080);
